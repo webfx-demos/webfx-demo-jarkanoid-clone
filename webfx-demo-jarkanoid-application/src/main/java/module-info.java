9cb9b122516450e36fa96f8eea00e0116dd15682
Bruno Salmon <dev.salmonb@gmail.com>
2023-02-19 20:07:34 +0000

    Added font loading wait before displaying start screen

--- webfx-demo-jarkanoid-application/src/main/java/module-info.java
+++ webfx-demo-jarkanoid-application/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module webfx.demo.jarkanoid.application {
     requires javafx.base;
     requires javafx.graphics;
     requires javafx.media;
+    requires webfx.kit.launcher;
     requires webfx.platform.resource;
     requires webfx.platform.scheduler;
     requires webfx.platform.shutdown;
